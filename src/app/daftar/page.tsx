8f1668b21471e14579fb4bf1f4361df62d19bdbd
rynnlf12 <148102850+rynnlf12@users.noreply.github.com>
2025-04-10 17:23:07 +0700

    fix : import Image from next/image

--- src/app/daftar/page.tsx
+++ src/app/daftar/page.tsx
@@ -10,6 +10,8 @@ import { Label } from '@/components/ui/label';
 import { RadioGroup, RadioGroupItem } from '@/components/ui/radio-group';
 import { Checkbox } from '@/components/ui/checkbox';
 import { Sun, Mountain, CloudSun } from 'lucide-react';
+import Image from 'next/image';
+
 
 const LOMBA_LIST = [
   { id: 'tandu_putra', name: 'Lomba Tandu Darurat Putra', biaya: 70000, orangPerTim: 2 },
