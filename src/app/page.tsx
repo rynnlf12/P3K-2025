b7fe8dfd25935bd5fce9db0f4a23b9c22038e629
rynnlf12 <148102850+rynnlf12@users.noreply.github.com>
2025-04-12 02:09:17 +0700

    updated error pages.tsx after adding navbarmobile and desktop

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,7 @@
 'use client';
 
 import { motion } from 'framer-motion';
-import Image from 'next/image';
 import { useEffect, useState } from 'react';
-import Link from 'next/link';
 
 export default function Home() {
   const [timeLeft, setTimeLeft] = useState({
