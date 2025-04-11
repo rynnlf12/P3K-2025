df44856296b66c02dd667c6a36e1d9b28f647a24
rynnlf12 <148102850+rynnlf12@users.noreply.github.com>
2025-04-12 02:30:00 +0700

    add icons

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,8 @@ import "./globals.css";
 
 import NavbarMobile from "@/components/NavbarMobile";
 import NavbarDesktop from "@/components/NavbarDesktop";
+import { FaWhatsapp, FaInstagram } from 'react-icons/fa';
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -53,14 +55,7 @@ export default function RootLayout({
             className="bg-green-500 text-white p-3 rounded-full shadow-lg hover:bg-green-600 transition"
             title="Kontak WhatsApp"
           >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-6 w-6"
-              fill="currentColor"
-              viewBox="0 0 24 24"
-            >
-              <path d="M12 0C5.373 0 0 5.373 0 12c0 2.09.532 4.053 1.464 5.774L0 24l6.486-1.684A11.955 11.955 0 0012 24c6.627 0 12-5.373 12-12S18.627 0 12 0z" />
-            </svg>
+            <FaWhatsapp className="w-6 h-6" />
           </a>
 
           <a
@@ -70,16 +65,10 @@ export default function RootLayout({
             className="bg-pink-500 text-white p-3 rounded-full shadow-lg hover:bg-pink-600 transition"
             title="Instagram"
           >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-6 w-6"
-              fill="currentColor"
-              viewBox="0 0 24 24"
-            >
-              <path d="M12 2.163c3.204 0 3.584.012 4.85.07 1.366.062 2.633.34 3.608 1.316.975.975 1.253 2.242 1.315 3.608.058 1.266.07 1.646.07 4.85s-.012 3.584-.07 4.85c-.062 1.366-.34 2.633-1.315 3.608-.975.975-2.242 1.253-3.608 1.315-1.266.058-1.646.07-4.85.07s-3.584-.012-4.85-.07c-1.366-.062-2.633-.34-3.608-1.315-.975-.975-1.253-2.242-1.315-3.608C2.175 15.584 2.163 15.204 2.163 12s.012-3.584.07-4.85c.062-1.366.34-2.633 1.315-3.608.975-.975 2.242-1.253 3.608-1.315C8.416 2.175 8.796 2.163 12 2.163zm0 10.162a3.999 3.999 0 110-7.998 3.999 3.999 0 010 7.998zM18.406 6.157a1.44 1.44 0 11-2.88 0 1.44 1.44 0 012.88 0z" />
-            </svg>
+            <FaInstagram className="w-6 h-6" />
           </a>
         </div>
+
       </body>
     </html>
   );
