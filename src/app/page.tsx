d4be66c0dbeb1a2cd4d4d6ca3eff0662ad33b62b
rynnlf12 <148102850+rynnlf12@users.noreply.github.com>
2025-04-12 00:41:33 +0700

    edit navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -69,7 +69,7 @@ export default function Home() {
       )}
 
       {/* Navbar */}
-      <div className="fixed top-4 left-1/2 -translate-x-1/2 w-[90%] max-w-5xl z-50 bg-white px-4 py-2 rounded-xl flex items-center justify-between border border-orange-300">
+      <div className="fixed top-4 left-1/2 -translate-x-1/2 w-full max-w-5xl z-50 bg-white px-1 py-1 rounded-xl flex items-center justify-between border border-orange-300">
         <div className="flex items-center gap-3">
           <Image
             src="/desain-p3k.png"
