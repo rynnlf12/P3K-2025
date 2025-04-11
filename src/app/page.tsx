41b6b54b80ab722ae245477db7fd26a259846f6c
rynnlf12 <148102850+rynnlf12@users.noreply.github.com>
2025-04-12 00:37:30 +0700

    ubah background navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -69,8 +69,7 @@ export default function Home() {
       )}
 
       {/* Navbar */}
-      {/* Navbar tanpa efek glassmorphism */}
-      <div className="fixed top-4 left-1/2 -translate-x-1/2 w-[95%] md:w-[90%] max-w-6xl z-50 bg-white px-6 py-3 rounded-full flex items-center justify-between border border-orange-300">
+      <div className="fixed top-4 left-1/2 -translate-x-1/2 w-[90%] max-w-5xl z-50 bg-white px-4 py-2 rounded-xl flex items-center justify-between border border-orange-300">
         <div className="flex items-center gap-3">
           <Image
             src="/desain-p3k.png"
