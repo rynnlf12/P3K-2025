5c56df22402941d647e47ae14341469144275cf7
rynnlf12 <148102850+rynnlf12@users.noreply.github.com>
2025-04-13 01:11:30 +0700

    fix error unduh

--- src/app/sukses/page.tsx
+++ src/app/sukses/page.tsx
@@ -1,12 +1,7 @@
 'use client';
-
 import dynamic from 'next/dynamic';
 
-// Import komponen utama secara dinamis agar hanya dirender di sisi client
-const SuksesContent = dynamic(() => import('./SuksesContent'), {
-  ssr: false,
-  loading: () => <div className="text-center py-32 text-orange-600">Memuat halaman...</div>,
-});
+const SuksesContent = dynamic(() => import('./SuksesContent'), { ssr: false });
 
 export default function Page() {
   return <SuksesContent />;
